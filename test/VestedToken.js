36cd28b52b3f2a4d22b78efc48759c272a1092e5
Mikhail Melnik <by.zumzoom@gmail.com>
2021-10-11 17:50:36 +0800

    linter

--- test/VestedToken.js
+++ test/VestedToken.js
@@ -1,4 +1,5 @@
-const { expect } = require("chai");
+const hre = require('hardhat');
+const { expect } = require('chai');
 const { ether, expectEvent, constants } = require('@openzeppelin/test-helpers');
 
 const INCH_ADDR = '0x111111111117dC0aa78b770fA6A738034120C302';
@@ -26,38 +27,38 @@ describe('VestedToken', async function () {
             await this.VestedToken.deregisterVestings(['0x32757Da4Bd73a35F05806Ee91CDB4B1746e63a45']),
             'VestingDeregistered',
             {
-                'vesting': '0x32757Da4Bd73a35F05806Ee91CDB4B1746e63a45',
-                'receiver': '0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B',
+                vesting: '0x32757Da4Bd73a35F05806Ee91CDB4B1746e63a45',
+                receiver: '0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B',
             },
         );
         expect(await this.VestedToken.balanceOf('0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B')).to.be.bignumber.equal(ether('44295000'));
         await hre.network.provider.request({
-            method: "hardhat_impersonateAccount",
+            method: 'hardhat_impersonateAccount',
             params: ['0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B'],
         });
 
         await hre.network.provider.request({
-            method: "hardhat_setBalance",
+            method: 'hardhat_setBalance',
             params: ['0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B', '0x' + ether('100').toString(16)],
         });
         await hre.network.provider.request({
-            method: "evm_setNextBlockTimestamp",
+            method: 'evm_setNextBlockTimestamp',
             params: [1638360000],
         });
         await hre.network.provider.request({
-            method: "evm_mine",
+            method: 'evm_mine',
         });
         const vesting = await IStepVesting.at('0x0ac6ff4dBdE9cEeDC6d4c08B14ceD9FF19A013aB');
         expect(await this.VestedToken.balanceOf('0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B')).to.be.bignumber.equal(ether('44295000'));
-        await vesting.claim({from: '0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B'});
+        await vesting.claim({ from: '0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B' });
         expect(await this.VestedToken.balanceOf('0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B')).to.be.bignumber.equal(ether('33221250'));
         expectEvent(
             await this.VestedToken.updateBalances(['0x0ac6ff4dBdE9cEeDC6d4c08B14ceD9FF19A013aB']),
             'Transfer',
             {
-                'from': '0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B',
-                'to': constants.ZERO_ADDRESS,
-                'value': ether('11073750').toString()
+                from: '0x4Dc06c025Cb97d6Dc4A17183187e28D297B83C7B',
+                to: constants.ZERO_ADDRESS,
+                value: ether('11073750').toString(),
             },
         );
     });
